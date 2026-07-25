72a4714617404b64d2378ee3634c35ff69db8ba3
2116040109 <2264682917@qq.com>
2026-07-26 02:17:06 +0800

    exp(A2.2): m-UNROLL-factor=4 d-UNROLL=16, build=1566 cycles, DSP=70, LUT=169151 (BEST)

--- hw/src/acc_top.cpp
+++ hw/src/acc_top.cpp
@@ -235,14 +235,14 @@ CB_LOAD:
 
 DIST_TABLE_BUILD:
     for (int c = 0; c < KS; c++) {
-        for (int mg = 0; mg < M_SYN; mg += 2) {
+        for (int mg = 0; mg < M_SYN; mg += 4) {
 #pragma HLS PIPELINE II=1
-            for (int i = 0; i < 2; i++) {
+            for (int i = 0; i < 4; i++) {
 #pragma HLS UNROLL
                 int m = mg + i;
                 float acc = 0.0f;
-                for (int d = 0; d < DS_SYN; d++) {
-#pragma HLS UNROLL
+                for (int d = 0; d < DS_SYN; d += 16) {
+#pragma HLS UNROLL factor=16
                     acc += sub_dist_l2(query[m * DS_SYN + d], codebook[m][c][d]);
                 }
                 dist_table[m][c] = acc;
